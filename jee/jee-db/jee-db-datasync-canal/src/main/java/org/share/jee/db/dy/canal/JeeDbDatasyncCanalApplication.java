2bb94e7a1e114825e01d5c0e4571160d67bb14d8
xingbm <1570178304@qq.com>
2021-01-30 13:53:47 +0800

    canal demo

---
+++ jee/jee-db/jee-db-datasync-canal/src/main/java/org/share/jee/db/dy/canal/JeeDbDatasyncCanalApplication.java
@@ -0,0 +1,13 @@
+package org.share.jee.db.dy.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeDbDatasyncCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeDbDatasyncCanalApplication.class, args);
+    }
+
+}
---
+++ jee/jee-db/jee-db-datasync-canal/src/test/java/org/share/jee/db/dy/canal/JeeDbDatasyncCanalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.share.jee.db.dy.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeDbDatasyncCanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
